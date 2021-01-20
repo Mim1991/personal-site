9b923eb1c7576ebe8104cc3c26fc92f324a898ad
Meehir Patel <meehir_patel@hotmail.co.uk>
2021-01-20 22:06:19 +0000

    images sliding in on order

--- app/javascript/components/banners.js
+++ app/javascript/components/banners.js
@@ -1,62 +1,68 @@
 const initBannerSlide = () => {
-  const bannerOne = document.querySelector(".top-stack");
-  const bannerTwo = document.querySelector(".second-stack");
-  const bannerThree = document.querySelector(".third-stack");
-  const bannerFour = document.querySelector(".fourth-stack");
+  window.onload = () => {
+    const bannerOne = document.querySelector(".slide-in");
+    console.dir(bannerOne.offsetWidth);
+    const bannerTwo = document.querySelector(".second-stack");
+    const bannerThree = document.querySelector(".third-stack");
+    const bannerFour = document.querySelector(".fourth-stack");
 
-  const bannerOneOffset =
-    bannerOne.getBoundingClientRect().left + bannerOne.offsetWidth;
-  const bannerTwoOffset =
-    bannerTwo.getBoundingClientRect().left + bannerTwo.offsetWidth;
-  const bannerThreeOffset =
-    bannerThree.getBoundingClientRect().left + bannerThree.offsetWidth;
-  const bannerFourOffset =
-    bannerFour.getBoundingClientRect().left + bannerFour.offsetWidth;
+    const bannerOneOffset =
+      bannerOne.getBoundingClientRect().left + bannerOne.offsetWidth;
 
-  window.addEventListener("scroll", () => {
-    let scroll = 0;
-    if (bannerOne.getBoundingClientRect().bottom > 0) {
-      scroll = window.pageYOffset;
-    }
-    console.log(scroll);
+    const bannerTwoOffset =
+      bannerTwo.getBoundingClientRect().left + bannerTwo.offsetWidth;
+    const bannerThreeOffset =
+      bannerThree.getBoundingClientRect().left + bannerThree.offsetWidth;
+    const bannerFourOffset =
+      bannerFour.getBoundingClientRect().left + bannerFour.offsetWidth;
 
-    // console.log(scroll, bannerOne.getBoundingClientRect().top);
+    window.addEventListener("scroll", () => {
+      let scroll = 0;
+      if (bannerOne.getBoundingClientRect().bottom > 0) {
+        scroll = window.pageYOffset;
+      }
+      // console.log(scroll);
 
-    const slideOneMoment =
-      scroll / (bannerOne.getBoundingClientRect().top + bannerOne.offsetHeight);
-    const slideTwoMoment =
-      scroll / (bannerTwo.getBoundingClientRect().top + bannerTwo.offsetHeight);
+      // console.log(scroll, bannerOne.getBoundingClientRect().top);
 
-    const slideThreeMoment =
-      scroll /
-      (bannerThree.getBoundingClientRect().top + bannerThree.offsetHeight);
-    const slideFourMoment =
-      scroll /
-      (bannerFour.getBoundingClientRect().top + bannerFour.offsetHeight);
+      const slideOneMoment =
+        scroll /
+        (bannerOne.getBoundingClientRect().top + bannerOne.offsetHeight);
+      const slideTwoMoment =
+        scroll /
+        (bannerTwo.getBoundingClientRect().top + bannerTwo.offsetHeight);
 
-    const slideOneFunction =
-      -bannerOneOffset + slideOneMoment * bannerOneOffset;
-    const slideTwoFunction =
-      -bannerTwoOffset + slideTwoMoment * bannerTwoOffset;
-    const slideThreeFunction =
-      -bannerThreeOffset + slideThreeMoment * bannerThreeOffset;
-    const slideFourFunction =
-      -bannerFourOffset + slideFourMoment * bannerFourOffset;
+      const slideThreeMoment =
+        scroll /
+        (bannerThree.getBoundingClientRect().top + bannerThree.offsetHeight);
+      const slideFourMoment =
+        scroll /
+        (bannerFour.getBoundingClientRect().top + bannerFour.offsetHeight);
 
-    if (slideOneFunction < 0 && slideOneFunction > -bannerOneOffset) {
-      bannerOne.style.transform = `translateX(${slideOneFunction}px)`;
-    }
+      const slideOneFunction =
+        -bannerOneOffset + slideOneMoment * bannerOneOffset;
+      const slideTwoFunction =
+        -bannerTwoOffset + slideTwoMoment * bannerTwoOffset;
+      const slideThreeFunction =
+        -bannerThreeOffset + slideThreeMoment * bannerThreeOffset;
+      const slideFourFunction =
+        -bannerFourOffset + slideFourMoment * bannerFourOffset;
 
-    if (slideTwoFunction < 0 && slideTwoFunction > -bannerTwoOffset) {
-      bannerTwo.style.transform = `translateX(${-slideTwoFunction}px)`;
-    }
-    if (slideThreeFunction <= 0 && slideThreeFunction > -bannerThreeOffset) {
-      bannerThree.style.transform = `translateX(${slideThreeFunction}px)`;
-    }
-    if (slideFourFunction <= 0 && slideFourFunction > -bannerFourOffset) {
-      bannerFour.style.transform = `translateX(${-slideFourFunction}px)`;
-    }
-  });
+      if (slideOneFunction < 0 && slideOneFunction > -bannerOneOffset) {
+        bannerOne.style.transform = `translateX(${slideOneFunction}px)`;
+      }
+
+      if (slideTwoFunction < 0 && slideTwoFunction > -bannerTwoOffset) {
+        bannerTwo.style.transform = `translateX(${-slideTwoFunction}px)`;
+      }
+      if (slideThreeFunction <= 0 && slideThreeFunction > -bannerThreeOffset) {
+        bannerThree.style.transform = `translateX(${slideThreeFunction}px)`;
+      }
+      if (slideFourFunction <= 0 && slideFourFunction > -bannerFourOffset) {
+        bannerFour.style.transform = `translateX(${-slideFourFunction}px)`;
+      }
+    });
+  };
 };
 
 export { initBannerSlide };
