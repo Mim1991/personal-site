5dc69058ec88c007fe5e37a01eff01dacf154185
Meehir Patel <meehir_patel@hotmail.co.uk>
2021-01-23 22:59:53 +0000

    fade in on scroll js

--- app/javascript/pages/threed.js
+++ app/javascript/pages/threed.js
@@ -2,10 +2,15 @@ const initScroll = () => {
   function servicesAnimate() {
     const appText = document.querySelector(".web-desc");
     const appTextPosition = appText.getBoundingClientRect().top;
-    const screenPosition = window.innerHeight / 2;
+    const designText = document.querySelector(".web-design-content");
+    const designTextPosition = designText.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.5;
 
     if (appTextPosition < screenPosition) {
       appText.classList.add("app-text-appear");
+    }
+    if (designTextPosition < screenPosition) {
+      designText.classList.add("app-text-appear");
     }
   }
 
