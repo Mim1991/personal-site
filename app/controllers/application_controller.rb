d4a1bf031c4765109953af4da12ffe8915353508
Meehir Patel <meehir_patel@hotmail.co.uk>
2021-01-26 21:19:23 +0000

    about me section layout and cv added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def download_pdf
+    send_file(
+      "#{Rails.root}/public/MeehirPatelResume.pdf",
+      filename: "MeehirPatelResume.pdf",
+      type: "application/pdf"
+    )
+  end
 end
