b507bdc1129950828bfdd200b4d911ca64ee763a
Meehir Patel <meehir_patel@hotmail.co.uk>
2021-01-29 16:09:21 +0000

    preloader no longer reloading links

--- app/javascript/pages/preloader.js
+++ app/javascript/pages/preloader.js
@@ -1,9 +1,20 @@
 const initPreloader = () => {
   const loading = document.querySelector(".preload");
-  console.log(loading);
-  window.addEventListener("load", () => {
-    loading.classList.add("preload-finish");
-  });
+  const waitingText = document.querySelector(".invis");
+
+  // window.addEventListener("load", (event) => {
+  //   event.preventDefault();
+  //   loading.classList.add("preload-finish");
+  //   loading.remove();
+  // });
+
+  window.onload = function () {
+    loading.style.display = "none";
+  };
+
+  setTimeout(function () {
+    waitingText.classList.add("not-invis");
+  }, 7000);
 };
 
 export { initPreloader };
