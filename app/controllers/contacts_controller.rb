78d731d5eef64bbcc1d464514bbfcab775f7b71e
Meehir Patel <meehir_patel@hotmail.co.uk>
2021-01-31 21:43:50 +0000

    rendered form on home page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,20 @@
 class ContactsController < ApplicationController
+  def new
+    @message = Contact.new
+  end
+
+  def create
+    @message = Contact.new(contact_params)
+    if @message.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :phone, :message)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   get "home/download_pdf"
 end
