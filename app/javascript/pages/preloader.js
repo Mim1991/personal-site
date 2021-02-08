28c3883fdf754be83d72397d874ee61365cdf464
Meehir Patel <meehir_patel@hotmail.co.uk>
2021-02-08 18:55:53 +0000

    loading page changed

--- app/javascript/pages/preloader.js
+++ app/javascript/pages/preloader.js
@@ -1,6 +1,9 @@
+import TypeIt from "typeit";
+
 const initPreloader = () => {
   const loading = document.querySelector(".preload");
   const waitingText = document.querySelector(".invis");
+  const loadingText = document.querySelector("#loading");
 
   // window.addEventListener("load", (event) => {
   //   event.preventDefault();
@@ -12,9 +15,22 @@ const initPreloader = () => {
     loading.style.display = "none";
   };
 
-  setTimeout(function () {
-    waitingText.classList.add("not-invis");
-  }, 7000);
+  new TypeIt(loadingText, {
+    strings: [
+      "Spinning up the hamster...⚙️",
+      "Charging flux capacitator...",
+      "Shovelling coal into the server...",
+      "Adding randomly mispeled words into text...",
+      "Discovering new ways to make you wait...⏳",
+      "Almost there...",
+    ],
+    speed: 20,
+    loop: false,
+  }).go();
+
+  // setTimeout(function () {
+  //   waitingText.classList.add("not-invis");
+  // }, 7000);
 };
 
 export { initPreloader };
